06cffcaca7a37c08a79053de29fef4c8dd051513
vbluelinav <ch646063@gmail.com>
2024-09-11 22:07:27 -0700

    bar chart

--- dashboard/src/components/App.jsx
+++ dashboard/src/components/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import LineChart from './LineChart.jsx'
-import '../../public/style.css'
+import LineChart from './LineChart.jsx';
+import BarChart from './BarChart.jsx';
+import Button from 'react-bootstrap/Button'
+import '../../public/style.css';
 
 const App = () => (
 
     <>
      <h1>Dashboard</h1>
-     <LineChart />
+     <BarChart />
+     <Button>test</Button>
     </>
-)
+) 
 
 export default App;
 
 {/* <RealTimeChart /> */}
+ {/* <LineChart /> */}
