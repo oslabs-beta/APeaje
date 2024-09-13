8a19f33a9575bfbb7921a23f17e425684b2325e9
felivalencia <nbyfceryhxet@hotmail.com>
2024-09-13 19:48:34 -0300

    Moved login reg and auth to Apeaje, still need to fix the server and test

---
+++ dashboard/src/components/Dashboard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useAuth } from './AuthContext';
+import { useNavigate } from 'react-router-dom';
+import App from './App'; 
+import Button from 'react-bootstrap/Button';
+
+const Dashboard: React.FC = () => {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    };
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Button onClick={handleLogout}>Logout</Button>
+            <App /> {/* here the graphs */}
+        </div>
+    );
+};
+
+export default Dashboard;
