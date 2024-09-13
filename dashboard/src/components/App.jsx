e6a3b9952f9d1a57cf724568ec90f73eb28bec18
vbluelinav <ch646063@gmail.com>
2024-09-12 19:33:36 -0700

    need to pull

---
+++ dashboard/src/components/App.jsx
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react';
+import BarChart from './BarChart.jsx'
+import '../../public/style.css'
+import Dashboard from '../pages/Dashboard.jsx'
+
+
+const App = () => {
+  <>
+    <h1>Dashboard</h1>
+    <Dashboard />
+ </>
+}
+
+export default App;
+
+
